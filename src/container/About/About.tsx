34685f588a7cec2015f3f46a8714885cc7bca63c
Noé Muñoz Sánchez <noesm.16@gmail.com>
2023-03-05 21:54:03 -0600

    Create HOC for social media and navigation & finish about section

--- src/container/About/About.tsx
+++ src/container/About/About.tsx
@@ -1,7 +1,62 @@
-import "./About.scss";
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
-const About = () => {
-  return <div>About</div>;
+import { AppWrap } from "../../wrapper";
+import "./About.scss";
+import { urlFor, client } from "../../client";
+
+type About = {
+  title: string;
+  description: string;
+  imgUrl: SanityImageSource;
 };
 
-export default About;
+const About = () => {
+  const [abouts, setAbouts] = useState<About[]>([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2 className="head-text">
+        I Know that <span>Good Design</span> <br />
+        means <span>Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl).url()} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+// export default AppWrap(
+//   MotionWrap(About, "app__about"),
+//   "about",
+//   "app__whitebg"
+// );
+
+export default AppWrap(About, "about");
--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { motion } from "framer-motion";
 
 import { AppWrap } from "../../wrapper";
@@ -68,4 +67,4 @@ const Header = () => (
   </div>
 );
 
-export default Header;
+export default AppWrap(Header, "home");
