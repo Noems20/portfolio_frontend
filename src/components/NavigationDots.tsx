707153229455dd93b14e0689ee1c582554941eca
Noé Muñoz Sánchez <noesm.16@gmail.com>
2023-03-08 20:59:41 -0600

    Fix social media & navigation dots z-index

--- src/components/NavigationDots.tsx
+++ src/components/NavigationDots.tsx
@@ -6,26 +6,22 @@ const NavigationDots = ({
   theme: string;
 }) => (
   <div className="app__navigation">
-    {[
-      "home",
-      "about",
-      "work",
-      "experiences",
-      "skills",
-      "testimonial",
-      "contact",
-    ].map((item, index) => (
-      <a
-        href={`#${item}`}
-        key={item + index}
-        className={`app__navigation-dot ${theme === "dark" ? "nightmode" : ""}`}
-        style={
-          active === item
-            ? { backgroundColor: theme === "dark" ? "#915eff" : "#313BAC" }
-            : {}
-        }
-      />
-    ))}
+    {["home", "about", "work", "experiences", "skills", "contact"].map(
+      (item, index) => (
+        <a
+          href={`#${item}`}
+          key={item + index}
+          className={`app__navigation-dot ${
+            theme === "dark" ? "nightmode" : ""
+          }`}
+          style={
+            active === item
+              ? { backgroundColor: theme === "dark" ? "#915eff" : "#313BAC" }
+              : {}
+          }
+        />
+      )
+    )}
   </div>
 );
 
